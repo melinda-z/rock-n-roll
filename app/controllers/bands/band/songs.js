28f440c9ae99ee18f61e1fbf5cda01f4742ab5a7
MelindaZhang2020 <melinda.zhang@phorest.com>
2023-02-14 09:23:35 +0000

    Add songs to the band and refactor model code

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,8 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { Song } from 'rarwe/routes/bands';
+import Song from 'rarwe/models/song';
+import { service } from '@ember/service';
 
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
@@ -10,6 +11,8 @@ export default class BandsBandSongsController extends Controller {
   get hasNoTitle() {
     return !this.title;
   }
+
+  @service catalog;
 
   @action
   updateTitle(event) {
@@ -20,6 +23,7 @@ export default class BandsBandSongsController extends Controller {
   saveSong() {
     let song = new Song({ title: this.title, band: this.model });
     this.model.songs = [...this.model.songs, song];
+    this.catalog.add('song', song);
     this.title = '';
     this.showAddSong = true;
   }
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
-export default class BandsBandSongsRoute extends Route {
+
+export default class Song {
+  constructor({ title, rating, band }) {
+  this.title = title;
+  this.rating = rating ?? 0;
+  this.band = band; } }
+  export default class BandsBandSongsRoute extends Route {
   resetController(controller) {
     controller.title = '';
     controller.showAddSong = true;
