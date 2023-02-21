de3289f3cdcb518ad301e67dff0790a5316e7beb
MelindaZhang2020 <melinda.zhang@phorest.com>
2023-02-21 10:16:21 +0000

    move bands and songs logic to backend

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,13 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-export default class Band { @tracked name; @tracked songs;
-constructor({ id, name, songs }) {
-this.id = id;
-this.name = name;
-this.songs = songs || [];
-} }
-
 export default class BandsBandRoute extends Route {
   @service catalog;
 
