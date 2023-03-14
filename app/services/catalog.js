dfd96d677bef7e69b1c9f140ef39108efd860189
MelindaZhang2020 <melinda.zhang@phorest.com>
2023-03-14 17:18:45 +0000

    fetching data from the backend, and loading records extracted from the response into the catalog

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,5 +1,15 @@
 import Service from '@ember/service';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
 import { tracked } from 'tracked-built-ins';
+
+function extractRelationships(object) {
+  let relationships = {};
+  for (let relationshipName in object) {
+    relationships[relationshipName] = object[relationshipName].links.related;
+  }
+  return relationships;
+}
 
 export default class CatalogService extends Service {
   storage = {};
@@ -8,6 +18,46 @@ export default class CatalogService extends Service {
     super(...arguments);
     this.storage.bands = tracked([]);
     this.storage.songs = tracked([]);
+  }
+
+  async fetchAll(type) {
+    if (type === 'bands') {
+      let response = await fetch('/bands');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.bands;
+    }
+
+    if (type === 'songs') {
+      let response = await fetch('/songs');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.songs;
+    }
+  }
+
+  loadAll(json) {
+    let records = [];
+    for (let item of json.data) {
+      records.push(this._loadResource(item));
+    }
+    return records;
+  }
+
+  _loadResource(data) {
+    let record;
+    let { id, type, attributes, relationships } = data;
+    if (type === 'bands') {
+      let rels = extractRelationships(relationships);
+      record = new Band({ id, ...attributes }, rels);
+      this.add('band', record);
+    }
+    if (type === 'songs') {
+      let rels = extractRelationships(relationships);
+      record = new Song({ id, ...attributes }, rels);
+      this.add('song', record);
+    }
+    return record;
   }
 
   add(type, record) {
