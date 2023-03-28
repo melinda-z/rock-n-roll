5632e9674f17b2dac6be1dff673a0ad4f8f63e2f
MelindaZhang2020 <melinda.zhang@phorest.com>
2023-03-28 10:21:00 +0100

    Add route changing functionality

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -8,16 +8,42 @@ export default class BandsNewController extends Controller {
   @service router;
 
   @tracked name;
+
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      if (transition.isAborted) {
+        return;
+      }
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   get hasNoName() {
     return !this.name;
   }
+
   @action
   updateName(event) {
     this.name = event.target.value;
   }
+
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
