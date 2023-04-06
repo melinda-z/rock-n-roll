97b22aa71bffd272dcefb0007d3faf50054efb5a
MelindaZhang2020 <melinda.zhang@phorest.com>
2023-04-06 10:12:51 +0100

    Add test cases

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click, fillIn, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'rarwe/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { getPageTitle } from 'ember-page-title/test-support';
@@ -22,6 +22,35 @@ module('Acceptance | bands', function (hooks) {
     assert.ok(
       bandLinks[1].textContent.includes('Long Distance Calling'),
       'The other band link contains the band name'
+    );
+  });
+
+  test('Create a band', async function (assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+    await visit('/');
+    await click('a[href="/bands/new"]');
+    await fillIn('input', 'Caspian');
+    await click('button');
+    await waitFor('p.text-center');
+
+    let bandLinks = document.querySelectorAll('.mb-2 > a');
+
+    // eslint-disable-next-line qunit/assert-args
+    assert.strictEqual(
+      bandLinks.length,
+      2,
+      'All band links are rendered',
+      'A new band link is rendered'
+    );
+    assert.ok(
+      bandLinks[1].textContent.includes('Caspian'),
+      'The new band link is rendered as the last item'
+    );
+    assert.ok(
+      document
+        .querySelector('.border-b-4.border-purple-400')
+        .textContent.includes('Songs'),
+      'The Songs tab is active'
     );
   });
 });
